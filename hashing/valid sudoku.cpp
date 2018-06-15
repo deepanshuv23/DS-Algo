a715b293981b6edc1ed309879d07abf95850e3c2
deepanshuv23 <deepanshu.tcl@gmail.com>
2018-06-15 09:41:15 +0530

    easy array hashing

---
+++ hashing/valid sudoku.cpp
@@ -0,0 +1,21 @@
+bool isValidSudoku(vector<string> &board) {
+        bool r[9][9], c[9][9], s[3][3][9];
+        memset(r, false, sizeof(r));
+        memset(c, false, sizeof(c));
+        memset(s, false, sizeof(s));
+        int number;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] > '0' && board[i][j] <='9') {
+                    number = board[i][j] - '1';
+                    if (r[i][number]) return false;             //if no in row
+                    r[i][number] = true;
+                    if (c[j][number]) return false;              // if no in col
+                    c[j][number] = true;
+                    if (s[i / 3][j / 3][number]) return false;      // if no in box
+                    s[i / 3][j / 3][number] = true;
+                }
+            }
+        }
+        return true;
+    }
