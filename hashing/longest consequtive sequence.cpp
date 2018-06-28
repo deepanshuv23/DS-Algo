ac2010fb6e74aec873fb6ddc2acdaeb95711ae1a
deepanshuv23 <deepanshu.tcl@gmail.com>
2018-06-28 23:30:54 +0530

    solution using maps not intuitive

---
+++ hashing/longest consequtive sequence.cpp
@@ -0,0 +1,76 @@
+
+int Solution::longestConsecutive(const vector<int> &A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    
+    unordered_map<int, bool> myMap;
+    
+    for(int i = 0; i < A.size(); i++){
+        myMap[A[i]] = false;
+    }
+    
+    auto it = myMap.begin();
+    
+    while(it != myMap.end()){
+        int req = it->first - 1;
+        if(myMap.find(req) == myMap.end()){
+            it->second = true;            
+        }
+        it++;
+    }
+    
+    int temp = 0, overall = 0;
+    
+    it = myMap.begin();
+    
+    while(it != myMap.end()){
+        if(it->second == true){
+            auto t = it;
+            while(t != myMap.end()){
+                int te = t->first + 1;
+                temp++;
+                t = myMap.find(te);
+            }
+            if(overall < temp){
+                overall = temp;
+            }
+            temp = 0;
+        }
+        it++;
+    }
+    
+    return overall;
+    
+}
+
+
+// not a fast solution
+int Solution::longestConsecutive(const vector<int> &A) 
+{
+    vector<int> ans(A.size());
+    ans=A;
+    
+    sort(ans.begin(),ans.end());
+    int n=A.size();
+    if(n==0)
+    {
+        return 0;
+    }
+    int count=1,max=0;
+    for(int i=0;i<n-1;i++)
+    {
+        while(A[i]+1==A[i+1])
+        {
+            count++;
+        }
+
+        if(count>max)
+            max=count;
+        
+        count=1;
+    }
+    return max;
+
+}
