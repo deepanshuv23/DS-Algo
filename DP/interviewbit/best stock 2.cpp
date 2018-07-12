b7a11a469be08bae7e3397bc3103f2827a302b29
deepanshuv23 <deepanshu.tcl@gmail.com>
2018-07-12 14:59:03 +0530

    slight variation

---
+++ DP/interviewbit/best stock 2.cpp
@@ -0,0 +1,22 @@
+int Solution::maxProfit(const vector<int> &A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+
+    if(A.size() == 0){
+        return 0;
+    }
+    
+    int sol = 0;
+    
+    for(int i = 1; i < A.size(); i++){
+       
+        if(A[i]-A[i-1]>0)
+            sol+=A[i]-A[i-1];
+        
+        
+    }
+    
+    return sol;
+}
