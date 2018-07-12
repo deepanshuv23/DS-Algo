dffff2c4b8f17b6f5259cddc70733ed72004637b
deepanshuv23 <deepanshu.tcl@gmail.com>
2018-07-12 15:06:45 +0530

    standard

---
+++ trees/inorder to cartsian tree.cpp
@@ -0,0 +1,35 @@
+int findMax(vector<int> &A, int start, int end){
+    int max = A[start];
+    int index = start;
+    for(int i = start; i <= end; i++){
+        if(max < A[i]){
+            max = A[i];
+            index = i;
+        }
+    }
+    return index;
+} 
+
+TreeNode* makeCart(vector<int> &A, int start, int end){
+    if(start > end){
+        return NULL;
+    }
+    
+    int maxIn = findMax(A, start, end);
+    
+    TreeNode* root = new TreeNode(A[maxIn]);
+    
+    root->left = makeCart(A, start, maxIn-1);
+    root->right = makeCart(A, maxIn+1, end);
+    
+    return root;
+}
+
+TreeNode* Solution::buildTree(vector<int> &A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    
+    return makeCart(A, 0, A.size()-1);
+}
